7f077f29796e835847ad5594a4e90ef6a2e79d17
PatrickSampa <sampa.mendonca@gmail.com>
2023-07-26 11:11:55 -0300

    finalizando erro 'file'

--- src/router.ts
+++ src/router.ts
@@ -3,17 +3,35 @@ import { setInformationUpdate } from "./modules/updateFile";
 import { multerConfig } from "./multer/multer";
 import { getInformationDatabaseController } from "./modules/getInformationDatabaseCsv";
 import { ClearDataBase } from "./repositories/prisma/prismaClearDataBase";
+import multer from "multer";
 
 
 const router = Router();
 
 
+function multerErrorHandling(err: any, req: any, res: any, next: any) {
+  if (err instanceof multer.MulterError) {
+    // Erro do multer - campo com nome diferente do esperado
+    return res.status(400).json({ error: "Invalid field name. Use 'file'." });
+  } else if (err) {
+    // Outro erro não esperado
+    return res.status(500).json({ error: "Error processing the file." });
+  }
+  // Se não houver erros, continue para o próximo middleware
+  next();
+}
 
 
-router.post("/updatefile", multerConfig.single("file"),(async (req, res) => {
-    return setInformationUpdate.handle(req,res)
-    
-}))
+
+router.post("/updatefile", multerConfig.single("file"), (req, res, next) => {
+  // Se o multer gerar um erro, o próximo middleware será o multerErrorHandling
+  next();
+}, async (req, res) => {
+  // Aqui você pode continuar o tratamento da rota após o multer
+  return setInformationUpdate.handle(req, res);
+});
+
+router.use(multerErrorHandling);
 
 router.get('/', async (req, res) => {
     return getInformationDatabaseController.handle(req, res);
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ app.use(cors())
 
 
 
+
 app.use(router)
 
 
